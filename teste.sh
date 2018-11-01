cc15286704f360b615758e2a5c41a056d864310f
Fsantos123 <44686168+Fsantos123@users.noreply.github.com>
2018-11-01 20:09:54 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#Listando o diretorios do Linux
+ls -lha /
+read
+clear
+
+#Atualizando o Apt ou Apt-Get
+apt updade
+echo "Listas atualizadas!!!"
+read
+cls
+
+#Desligando o servidor
+shutdown -h now
